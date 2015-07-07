ef9218c1e7582a7f9a27dd787bb74ec5e8ffbcad
tysweezy <tsouzs.sf@gmail.com>
2015-07-07 15:36:20 -0700

    added empty test -- also triggering build

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+# Decipher API Notes
+
+Looks like **x-apikey** is a HTTP header that handles the actual Decipher API key.
+
---
+++ tests/HttpStatusTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Decipher\Api\Test;
+
+class HttpStatusTest extends \PHPUnit_Framework_TestCase {
+
+	public function testIfUrlIsOkay()
+	{
+		
+	}
+}
